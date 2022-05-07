db3c21c455e285279846da2e2e0def9dbdd1083c
vlnarasimham <105101463+vlnarasimham@users.noreply.github.com>
2022-05-07 13:55:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit
+import pandas 
 
 streamlit.title('Nar is learning Streamlit')
 streamlit.header ('Nar is enjoying Streamlit') 
 
 streamlit.text('hi, this is Nar , learning snowflake')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
+
