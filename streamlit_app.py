64ee5fdb98a0d590a039ffaa8d861c749c7f754a
vlnarasimham <105101463+vlnarasimham@users.noreply.github.com>
2022-05-07 18:17:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,8 @@ streamlit.header ('Nar is enjoying Streamlit')
 #streamlit.text('hi, this is Nar , learning snowflake')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:",  list(my_fruit_list.index),['Avocado', 'Strawberries'])
 streamlit.dataframe(my_fruit_list)
 
-#fruits_selected = streamlit.multiselect("Pick some fruits:",  list(my_fruit_list.index),['Avocado', 'Strawberries'])
-#fruits_to_show = my_fruit_list.loc[fruits_selected]
-#streamlit.datafrmae(fruits_to_show)
+fruits_selected = streamlit.multiselect("Pick some fruits:",  list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
