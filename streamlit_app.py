2bf7290e269f5f65517f0843e52fe961963cdaf6
vlnarasimham <105101463+vlnarasimham@users.noreply.github.com>
2022-05-07 17:57:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.title('Nar is learning Streamlit')
 streamlit.header ('Nar is enjoying Streamlit') 
 
 #streamlit.text('hi, this is Nar , learning snowflake')
-#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #streamlit.dataframe(my_fruit_list)
 streamlit.multiselect("Pick some fruits:",  list(my_fruit_list.index))
 
