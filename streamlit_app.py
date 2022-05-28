ccd77ed4850e1ae83f11acfb1ecbd6cd7347823d
vlnarasimham <105101463+vlnarasimham@users.noreply.github.com>
2022-05-28 16:39:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas 
+import snowflake.connector 
 
 streamlit.title('Nar is learning Streamlit')
 streamlit.header ('Nar is enjoying Streamlit') 
@@ -12,3 +13,17 @@ streamlit.dataframe(my_fruit_list)
 fruits_selected = streamlit.multiselect("Pick some fruits:",  list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = mycnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone() 
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+
+
+
+
+
+
